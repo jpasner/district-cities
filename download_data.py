425692eb47c034f3901da577ac714c6aeb5a9d24
Jake Pasner <jakepasner@gmail.com>
2025-03-22 14:51:05 -0700

    Add script to download and unzip National Gazetteer and Census Incorporated Places files

--- download_data.py
+++ download_data.py
@@ -1,8 +1,39 @@
 import os
 import requests
+import zipfile
 from bs4 import BeautifulSoup
 
-# URL of the TIGER 2023 Congressional District shapefiles
+# Step1: Download the 2023 National Gazetteer Places File and unzip it to the `data` directory
+gazetteer_url = "https://www2.census.gov/geo/docs/maps-data/data/gazetteer/2023_Gazetteer/2023_Gaz_place_national.zip"
+gazetteer_zip_path = "data/2023_Gaz_place_national.zip"
+os.makedirs("data", exist_ok=True)
+
+print(f"Downloading National Gazetteer Places File from {gazetteer_url} ...")
+with requests.get(gazetteer_url, stream=True) as r:
+    r.raise_for_status()
+    with open(gazetteer_zip_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
+print("Downloaded National Gazetteer Places File.")
+
+print("Unzipping the National Gazetteer Places File ...")
+with zipfile.ZipFile(gazetteer_zip_path, "r") as zip_ref:
+    zip_ref.extractall("data")
+# Unzipped National Gazetteer Places File.
+print("Unzipped National Gazetteer Places File.")
+
+# Step 2: Download the 2023 Census file of Incorporated Places and save it to the data directory
+incorporated_places_url = "https://www2.census.gov/programs-surveys/popest/tables/2020-2023/cities/totals/SUB-IP-EST2023-POP.xlsx"
+incorporated_places_path = "data/SUB-IP-EST2023-POP.xlsx"
+print(f"Downloading 2023 Census Incorporated Places file from {incorporated_places_url} ...")
+with requests.get(incorporated_places_url, stream=True) as r:
+    r.raise_for_status()
+    with open(incorporated_places_path, "wb") as f:
+         for chunk in r.iter_content(chunk_size=8192):
+             f.write(chunk)
+print("Downloaded 2023 Census Incorporated Places file.")
+
+# Step 3:Download TIGER 2023 Congressional District shapefiles
 base_url = "https://www2.census.gov/geo/tiger/TIGER2023/CD/"
 
 # Send a request to the URL and parse the HTML content
